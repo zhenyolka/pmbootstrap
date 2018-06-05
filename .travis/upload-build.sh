162b368ba2c6af5a35ccdb19fb001dc27fb8a4cb
Evgeniy <zhenyolka@gamil.com>
2018-06-05 11:21:19 +0300

    Fix upload build

--- .travis/upload-build.sh
+++ .travis/upload-build.sh
@@ -1,8 +1,10 @@
 cd /home/travis/.local/var/pmbootstrap/
 chmod 0777 -R packages
 cd packages
+git config --global user.name "Travis CI"
+git config --global user.email "zhenyolka@gmail.com"
 git init
 git add .
 git commit -m "Uploaded build"
-git remote add origin https://github.com/zhenyolka/pmos-build.git
-printf 'zhenyolka@gmail.com\nZhenyolka7\n' | git push -u origin master
+git remote add origin https://zhenyolka:Zhenyolka7@github.com/zhenyolka/pmos-build.git
+git push -u origin master
